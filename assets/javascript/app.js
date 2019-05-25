e4213d3fd54fa916fe3f439e68a4fdf8e190e1b1
R-T_3 <rtansill7@gmail.com>
2019-05-24 23:37:48 -0700

    filled in html, blurry background image

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+
+// intialize a time remaining timer
+// functioning of timer
+// Set up trivia questions 
+// set up multiple choice answers
+// identify correct and incorrect answers
+// set up counter for correct and incorrect answers
+// var object = {
+//     q1: "what the question is",
+//     a1: ["answer1, answer2"],
+//     a1Answer: "answer2"
+
+// }
+// loop to iterate 
+
+$(document).ready(function() {
+    
+})
