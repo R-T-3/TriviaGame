7fb95c287cd1887b7929bc6f6d938351ae83a97b
R-T_3 <rtansill7@gmail.com>
2019-05-29 14:50:23 -0700

    majority of js complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,3 @@
-
-// intialize a time remaining timer
-// functioning of timer
-// Set up trivia questions 
-// set up multiple choice answers
-// identify correct and incorrect answers
-// set up counter for correct and incorrect answers
-// var object = {
-//     q1: "what the question is",
-//     a1: ["answer1, answer2"],
-//     a1Answer: "answer2"
-
-// }
-// loop to iterate 
-
 window.onload = function() {
   $("#start").on("click", startGame);
   $("#done").on("click", done);
@@ -23,8 +8,55 @@ window.onload = function() {
   var clockRunning = false;
 
 function startGame() {
-    $("#questions").removeClass("hide");
+    $("#content").removeClass("hide");
     $("#start").addClass("hide");
+    gameQuestions();
+    run();
+}
+
+function run() {
+  clearInterval(intervalID);
+  intervalId = setInterval(decrement, 1000);
+}
+
+function decrement() {
+  secondsLeft--;
+  $("#timeRemaining").html("<h2>" + 'Time Remaining: ' + secondsLeft + "</h2>");
+
+  if (secondsLeft === 0) {
+    done();
+    alert("Time's Up!");
+  }
+}
+
+function done(){
+  clearInterval(intervalID);
+}
+
+var triviaQuestions = [
+   {
+     Question:"1.) Who holds the all-time record for most points scored in the NBA?",
+     Options: ["Kobe Bryant", "Lebron James", "Kareem Abdul Jabbar", "Michael Jordan"],
+     Answer: "Kareem Abdul Jabbar"
+   },
+   {
+    Question:"2.) What NFL team has won the most Superbowl's?",
+    Options: ["Dallas Cowboys", "Pittsburgh Steelers", "San Francisco 49ers", "Greenbay Packers"],
+    Answer: "Pittsburgh Steelers"
+  }
+]
+
+function gameQuestions() {
+  for (var i = 0; i < triviaQuestions.length; i++) {
+    var triviaQ = $("#questions").append("<h2>" + triviaQuestions[i].Question + "</h2>");
+      
+    for (var j = 0; j < triviaQuestions[i].Options.length; j++){
+      var optionEle = triviaQuestions[i].Options;
+      var eachOption = optionEle[j];
+        
+      triviaQ.append("<input type='radio' name='radio_btn'>" + eachOption + "</input>");   
+      }
+  }
 }
 
 
